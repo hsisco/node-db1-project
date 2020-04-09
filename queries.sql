69231fff1d1f383b289929171212028156a1ae2b
hsisco <hysensisco@gmail.com>
2020-04-08 22:38:10 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -13,17 +13,17 @@ SELECT * FROM [Orders]
 ORDER BY OrderDate desc limit 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
-SELECT * FROM Customers
+SELECT * FROM [Customers]
 WHERE City='London' OR City='Madrid' OR Country='Brazil';
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
-INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+INSERT INTO [Customers] (CustomerName, ContactName, Address, City, PostalCode, Country)
 VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
-UPDATE Customers
-SET PostalCode = 11122
-WHERE ContactName = 'Bilbo Baggins';
+UPDATE [Customers]
+SET PostalCode=11122
+WHERE ContactName='Bilbo Baggins';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
